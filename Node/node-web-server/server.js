6a888df656272c314f1243e38ca0fedc87d7d346
oadhali <oadhali@ainq.com>
2019-02-27 19:47:45 -0500

    express intro

---
+++ Node/node-web-server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const hbs = require('hbs');
+
+let app = express();
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the website',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.listen(3000);
