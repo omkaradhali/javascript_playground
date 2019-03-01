fe094516388dc876d52460a7c23c349ff7f8b686
oadhali <oadhali@ainq.com>
2019-02-28 22:46:47 -0500

    how to use express middlewares

--- Node/node-web-server/server.js
+++ Node/node-web-server/server.js
@@ -1,23 +1,53 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
+
+//Handlebars.
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+//Setting handlebars.
 app.set('view engine', 'hbs');
+
+//Middlewares.
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now} : ${req.method} ${req.url}`;
+  fs.appendFile('server.log' , log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  })
+  console.log(`${now} : ${req.method} ${req.url}`);
+  next();
+});
+
+//Maintenace page (middleware to halt everything else.)
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
+//Routes.
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to the website',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
